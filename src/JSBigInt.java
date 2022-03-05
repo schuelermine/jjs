60c8ed978ac75353aab81f8e584fbbc21c19d5db
Anselm Schüler <mail@anselmschueler.com>
2022-03-05 11:44:23 +0100

    Add arithmetic and BigInt

---
+++ src/JSBigInt.java
@@ -0,0 +1,53 @@
+import java.math.BigInteger;
+
+public class JSBigInt extends JSValue {
+    public JSBigInt(BigInteger value) {
+        this.value = value;
+    }
+
+    private BigInteger value;
+
+    public BigInteger get() {
+        return this.value;
+    }
+
+    public static JSBigInt add(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().add(value2.get()));
+    }
+
+    public static JSBigInt subtract(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().subtract(value2.get()));
+    }
+
+    public static JSBigInt multiply(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().multiply(value2.get()));
+    }
+
+    public static JSBigInt divide(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().divide(value2.get()));
+    }
+
+    public static JSBigInt remainder(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().remainder(value2.get()));
+    }
+
+    public static JSBigInt shiftLeft(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().shiftLeft(value2.get().intValue()));
+    }
+
+    public static JSBigInt shiftRight(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().shiftRight(value2.get().intValue()));
+    }
+
+    public static JSBigInt and(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().and(value2.get()));
+    }
+
+    public static JSBigInt or(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().or(value2.get()));
+    }
+
+    public static JSBigInt xor(JSBigInt value1, JSBigInt value2) {
+        return new JSBigInt(value1.get().xor(value2.get()));
+    }
+}
--- src/JSNumber.java
+++ src/JSNumber.java
@@ -8,4 +8,44 @@ public class JSNumber extends JSValue {
     public double get() {
         return this.value;
     }
+
+    public static JSNumber add(JSNumber value1, JSNumber value2) {
+        return new JSNumber(value1.get() + value2.get());
+    }
+
+    public static JSNumber subtract(JSNumber value1, JSNumber value2) {
+        return new JSNumber(value1.get() - value2.get());
+    }
+
+    public static JSNumber multiply(JSNumber value1, JSNumber value2) {
+        return new JSNumber(value1.get() * value2.get());
+    }
+
+    public static JSNumber divide(JSNumber value1, JSNumber value2) {
+        return new JSNumber(value1.get() / value2.get());
+    }
+
+    public static JSNumber remainder(JSNumber value1, JSNumber value2) {
+        return new JSNumber(value1.get() % value2.get());
+    }
+
+    public static JSNumber shiftLeft(JSNumber value1, JSNumber value2) {
+        return new JSNumber((int) value1.get() << (int) value2.get());
+    }
+
+    public static JSNumber shiftRight(JSNumber value1, JSNumber value2) {
+        return new JSNumber((int) value1.get() >> (int) value2.get());
+    }
+
+    public static JSNumber and(JSNumber value1, JSNumber value2) {
+        return new JSNumber((int) value1.get() & (int) value2.get());
+    }
+
+    public static JSNumber or(JSNumber value1, JSNumber value2) {
+        return new JSNumber((int) value1.get() | (int) value2.get());
+    }
+
+    public static JSNumber xor(JSNumber value1, JSNumber value2) {
+        return new JSNumber((int) value1.get() ^ (int) value2.get());
+    }
 }
