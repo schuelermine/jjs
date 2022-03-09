e69cf95471202e51e8f906e03614cfa0018fbfac
Anselm Schüler <mail@anselmschueler.com>
2022-03-09 21:56:48 +0100

    more

---
+++ src/JSSymbolFactory.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.UUID;
+
+public class JSSymbolFactory {
+    public JSSymbolFactory(Set<UUID> takenUUIDs) {
+        this.takenUUIDs = takenUUIDs;
+    }
+
+    private Set<UUID> takenUUIDs;
+
+    public JSSymbol mkSymbol(JSString name) {
+        UUID uuid = UUID.randomUUID();
+        this.takenUUIDs.add(uuid);
+        return new JSSymbol(name, uuid);
+    }
+}
