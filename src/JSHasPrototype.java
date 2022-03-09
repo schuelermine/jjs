f4dcc5abad5af05dfa5c53bca19a8aa6cee21036
Anselm Schüler <mail@anselmschueler.com>
2022-03-09 20:47:47 +0100

    Revert "fubarg"
    This reverts commit 9a7b1a7491abad6da4ca7363ca99fc97a4550430.

--- src/JSHasPrototype.java
+++ src/JSHasPrototype.java
@@ -12,13 +12,4 @@ public abstract class JSHasPrototype extends JSValue {
     public JSObject getPrototype() {
         return this.prototype;
     }
-    
-
-    public JSValue getPrototypeProperty(JSString jsKey) throws JSValue {
-        return this.prototype.getProperty(jsKey);
-    }
-
-    public JSValue callPrototypeMethod(JSString jsKey) throws JSValue {
-
-    }
 }
