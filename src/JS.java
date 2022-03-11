022dee39c8b2a43b4e4cee2a4ebdf5f5ef485c51
Anselm Schüler <mail@anselmschueler.com>
2022-03-11 15:50:24 +0100

    JS

--- src/JS.java
+++ src/JS.java
@@ -5,20 +5,55 @@ public class JS {
     private static JSObject objectPrototype = new JSObject(null, null);
 
     public static JSObject createObject(HashMap<String,JSPropertyDeclaration> entries) {
-        JSObject prototype = null
+        JSObject prototype = null;
         return new JSObject(entries, prototype);
     }
 
-    public static JSObject globalThis = createObject();
+    public static JSObject globalThis = createObject(null);
 
     public class JSPropertyDeclaration {
-        private JSPropertyDeclarationType kind;
-    
-        public enum JSPropertyDeclarationType {
-            Value,
-            Getter,
-            Setter
+        public JSPropertyDeclaration(String name, JSValue value) {
+            if (name == null || value == null) {
+                throw new NullPointerException();
+            }
+            this.type = JSPropertyType.DataProperty;
+            this.name = name;
+            this.value = value;
+            this.getter = null;
+            this.setter = null;
+        }
+
+        public JSPropertyDeclaration(String name, JSFunction get, JSFunction set) {
+            if (name == null || value == null) {
+                throw new NullPointerException();
+            }
+            this.type = JSPropertyType.AccessorProperty;
+            this.name = name;
+            this.value = null;
+            this.getter = get;
+            this.setter = set;
+        }
+
+        private final String name;
+        private final JSValue value;
+        private final JSFunction getter;
+        private final JSFunction setter;
+        private final JSPropertyType type;
+
+        public String getName() {
+            return this.name;
+        }
+
+        public JSValue getValue() {
+            return this.value;
+        }
+
+        public JSFunction getGet() {
+            return this.getter;
+        }
+
+        public JSFunction getSet() {
+            return this.setter;
         }
     }
-    
 }
