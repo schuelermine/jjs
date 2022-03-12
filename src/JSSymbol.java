d92c9bf8626157e2ca9329042d192a2d8714fcd8
Anselm Schüler <mail@anselmschueler.com>
2022-03-12 12:56:29 +0100

    I think I will re-do the entire thing

--- src/JSSymbol.java
+++ src/JSSymbol.java
@@ -2,16 +2,16 @@ import java.util.Objects;
 import java.util.UUID;
 
 class JSSymbol {
-    public JSSymbol(JSString name, UUID uuid) {
-        this.name = name.getString();
+    public JSSymbol(String name, UUID uuid) {
+        this.name = name;
         this.uuid = uuid;
     }
 
     private final String name;
     private final UUID uuid;
 
-    public JSString getName() {
-        return new JSString(this.name);
+    public String getName() {
+        return this.name;
     }
 
     @Override
